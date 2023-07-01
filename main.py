062393f2a84bb0960c96662f654f7e68508a0e22
C1ct0r <c1ct0r@icloud.com>
2023-07-01 09:59:25 +0200

    v0.2

--- main.py
+++ main.py
@@ -1,88 +1,72 @@
 import mysql.connector
-from tensorflow_process_data import getdata, evaluatedata
-from flask import Flask, render_template, redirect, request, jsonify, session
+from flask import Flask, render_template, redirect, jsonify
 from datetime import date
 
 app = Flask(__name__)
-app.secret_key = 'session_secret_key bad_secret_key'
 
-@app.route("/api/token")
-def token_api():
-    clientname = request.headers['clientname']
-    clientkey = request.headers['clientkey']
+db_host = "127.0.0.1"
+db_user = "root"
+db_password = "mysqldev"
+db_port = "3307"
+db_database = "cryptoai"
 
+def db_setup():
     db = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='mysqldev',
-        database='tensorflow',
-        port='3307'
+        host = db_host,
+        user = db_user,
+        password = db_password,
+        port = db_port
     )
 
     cursor = db.cursor(buffered=True)
-
-    cursor.execute("SELECT token FROM backend WHERE clientname = %s AND clientkey = %s", (clientname, clientkey, ))
-    token = cursor.fetchone()
-    if token == None:
-        return jsonify({'token': 'falsetoken'})
-    else:
-        token = token[0]
-
+    
+    cursor.execute("CREATE DATABASE IF NOT EXISTS cryptoai")
+    
     db.close()
-
-    return jsonify({'token': token})
-
-@app.route("/api/prediction")
-def predict_api():
-    token = request.headers['token']
-    authkey = request.headers['authkey']
-    crypto = request.headers['coin']
-    fiat = request.headers['fiat']
-
+    
     db = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='mysqldev',
-        database='tensorflow',
-        port='3307'
+        host = db_host,
+        user = db_user,
+        password = db_password,
+        port = db_port,
+        database = db_database
     )
-
+    
     cursor = db.cursor(buffered=True)
 
-    cursor.execute("SELECT token FROM backend WHERE authkey = %s", (authkey, ))
-    token_sql = cursor.fetchone()
-    token_sql = token_sql[0]
-
+    cursor.execute("CREATE TABLE IF NOT EXISTS crypto (cryptoID INT, levelID INT, coin TEXT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS exchange (exchangeID INT, levelID INT, exchangename TEXT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS discount (discountID INT, discountname TEXT, price FLOAT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS fiat (fiatID INT, levelID INT, currency TEXT, symbol TEXT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS level (levelID INT, levelname TEXT, price FLOAT, predictions INT, api INT, ticket TEXT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS model (modelID INT, levelID INT, cryptoID INT, version FLOAT, modelname TEXT, accuracy FLOAT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS payment (paymentID INT, userID INT, levelID INT, paymenttypeID INT, days INT, price FLOAT, timestamp BIGINT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS paymenttype (paymenttypeID INT, paymenttypename TEXT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS prediction (predictionID INT, modelID INT, fiatID INT, exchangeID INT, cryptoID INT, timestamp BIGINT, price FLOAT, rsi FLOAT, lowerbb FLOAT, upperbb FLOAT, value INT, timeframe TEXT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS user (userID INT, levelID INT, username TEXT, password TEXT, email TEXT, token TEXT, apitoken TEXT, apisecret TEXT, predictions INT, days INT, verified INT, code INT, codetimestamp BIGINT)")
+    db.commit()
+    cursor.execute("CREATE TABLE IF NOT EXISTS ticket (ticketID INT, userID INT, reference TEXT, body TEXT, edited INT)")
+    db.commit()
+    
     db.close()
 
-    if token == token_sql:
-        db = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='mysqldev',
-        database='tensorflow',
-        port='3307'
-        )
-        
-        cursor = db.cursor(buffered=True)
-
-        cursor.execute("SELECT level FROM backend WHERE authkey = %s", (authkey, ))
-        level = cursor.fetchone()
-        level = level[0]
-
-        getdata(crypto, fiat, 'binance', '1d')
-        evaluatedata(crypto, fiat, 'binance', '1d', level)
-
-        return jsonify({'priced': '123443',
-                        'priceh': '34233',
-                        'predictiond': '2',
-                        'predictionh': '1',
-                        'model': 'BTC1-1'})
-    else:
-        return jsonify({'error': 'authkey and token not matching'})
+db_setup()
 
 @app.route("/")
-def route1():
-    return render_template("chart.html")
+def main():
+    return redirect("/charts")
+
+@app.route("/charts")
+def charts():
+    return render_template("charts.html")
 
 if __name__ == '__main__': app.run(debug=True, port=8000, host='0.0.0.0')
